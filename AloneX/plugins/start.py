9e1cdfdbda4d3a1c61582abf000177fd91488fb2
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2023-12-04 22:09:58 +0530

    Update start.py

--- AloneX/plugins/start.py
+++ AloneX/plugins/start.py
@@ -42,7 +42,7 @@ async def start_comm(client, message: Message, _):
         name = message.text.split(None, 1)[1]
         if name[0:4] == "help":
             keyboard = help_pannel(_)
-            await message.reply_sticker("a03a705c868546a683e81")
+            await message.reply_sticker("https://te.legra.ph/file/96719e68b4b22e91c3f7c.png")
             return await message.reply_photo(
                        photo=config.START_IMG_URL,
                        caption=_["help_1"].format(config.SUPPORT_HEHE), reply_markup=keyboard
@@ -210,7 +210,7 @@ async def start_comm(client, message: Message, _):
         served_chats = len(await get_served_chats())
         served_users = len(await get_served_users())
         try:
-            await message.reply_sticker("a03a705c868546a683e81")
+            await message.reply_sticker("https://te.legra.ph/file/96719e68b4b22e91c3f7c.png")
             await message.reply_photo(
                 photo=image,
                 caption=_["start_2"].format(
